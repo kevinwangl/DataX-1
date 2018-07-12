84d519a7a7f8cce9a14275116a7729edde0ba85f
wangl <kevin.kyo@gmail.com>
2018-07-12 12:29:40 +0800

    Update MongoDBReader.java
    修正读入数据为嵌套对象时全部转换为标准json字符串

--- mongodbreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbreader/MongoDBReader.java
+++ mongodbreader/src/main/java/com/alibaba/datax/plugin/reader/mongodbreader/MongoDBReader.java
@@ -171,10 +171,17 @@ public class MongoDBReader extends Reader {
                             } else {
                                 ArrayList array = (ArrayList)tempCol;
                                 String tempArrayStr = Joiner.on(splitter).join(array);
+                                tempArrayStr = fixedNestedDocument(column,tempCol);
                                 record.addColumn(new StringColumn(tempArrayStr));
                             }
                         } else {
-                            record.addColumn(new StringColumn(tempCol.toString()));
+
+//                            record.addColumn(new StringColumn(tempCol.toString()));
+                            String column_value = fixedNestedDocument(column, tempCol);
+
+
+                            record.addColumn(new StringColumn(column_value));
+                            System.out.println("[INFO] ------------------------------------------------------------------------ fix end");
                         }
                     }
                 }
@@ -208,5 +215,39 @@ public class MongoDBReader extends Reader {
 
         }
 
+    }
+
+    /**
+     *修复当mongodb数据是嵌套结构时将数据转换为json字符串
+     * @param column 源数据表的列元数据
+     * @param tempCol 源数据列的值
+     * @return
+     */
+    private static String fixedNestedDocument(JSONObject column, Object tempCol) {
+        //如果数据是个嵌套级对象直接转为json格式的string
+//        System.out.println("[INFO] ------------------------------------------------------------------------ fix nestedDocument");
+//        String column_name = column.getString(KeyConstant.COLUMN_NAME);
+//        System.out.println("[INFO] ------------------------------------------------------------------------current column "+column_name);
+        String column_value = "";
+        if(tempCol!=null){
+//            System.out.println("[INFO] ------------------------------------------------------------------------current column value class "+tempCol.getClass());
+            if(tempCol instanceof Document){
+                column_value = ((Document) tempCol).toJson();
+            }else if(tempCol instanceof ArrayList){
+                ArrayList<Document> array = (ArrayList<Document>) tempCol;
+                JSONArray jsonArray = new JSONArray();
+                for(int i=0;i<array.size();i++){
+                    Document element = array.get(i);
+                    JSONObject json = JSONObject.parseObject(element.toJson());
+                    jsonArray.add(json);
+                }
+                column_value = jsonArray.toJSONString();
+            }else{
+                column_value = tempCol.toString();
+            }
+//            System.out.println("[INFO] ------------------------------------------------------------------------current data is "+column_value);
+        }
+
+        return column_value;
     }
 }
